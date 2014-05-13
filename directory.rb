f268b5adf79e50fc56ad9b8d80112bf21a4e177f
Roi Driscoll <roi.driscoll@gmail.com>
2014-05-13 13:41:39 +0100

    Refactor using a loop to output array items

--- directory.rb
+++ directory.rb
@@ -24,25 +24,10 @@ students = [
 #And then print them
 puts "Makers Academy : May 2014 Cohort"
 puts "================================"
-puts students[0]
-puts students[1]
-puts students[2]
-puts students[3]
-puts students[4]
-puts students[5]
-puts students[6]
-puts students[7]
-puts students[8]
-puts students[9]
-puts students[10]
-puts students[11]
-puts students[12]
-puts students[13]
-puts students[14]
-puts students[15]
-puts students[16]
-puts students[17]
-puts students[18]
+
+students.each do |student|
+    puts student
+end
 
 #Finally we print the total
 puts "Overall we have #{students.length} great students"
